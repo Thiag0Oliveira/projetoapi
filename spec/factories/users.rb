8ffb220fc64754d659961fade0c77a55049e4e33
Thiago Oliveira <contato.thg@hotmail.com>
2019-03-26 21:05:49 +0000

    improving rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,21 @@
-Factorybot.define do
+FactoryBot.define do
 
-  factory user: do
-    email "contato.tgh@hotmail.com"
-    password "123456"
-    password_confirmation "123456"
+ factory :user do
+  
+   email {Faker::Internet.email}#{"contato.tgh@hotmail.com"}
+   password  { '123456' }
+   password_confirmation  {'123456' } 
+   #encrypted_password {'123456'}
+  #  password_confirmation "123456"
   end
 end
+
+##FactoryBot.define do
+ # factory :user do
+  #  email  {"contato.tgh@hotmail.com"} 
+ #   password  {"123456" }
+  #  password_confirmation  {"123456"} 
+     
+ # end
+#end
+#end
